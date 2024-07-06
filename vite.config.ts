b3d38902ebfe535e8b7ab44666d24ca9f67df4c9
joshradin <jradin16@gmail.com>
2024-07-05 20:58:41 -0700

    made base absolute

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://joshradin.github.io/clippy_clicker',
+  base: 'https://joshradin.github.io/clippy_clicker/',
   plugins: [react()],
 });
